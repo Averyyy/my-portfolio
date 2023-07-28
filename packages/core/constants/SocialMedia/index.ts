bfacbdb4a95ad43438d76604d5b59431426526f8
Averyyy <58547284+Averyyy@users.noreply.github.com>
2023-07-28 15:27:39 +0800

    first commit

---
+++ packages/core/constants/SocialMedia/index.ts
@@ -0,0 +1,12 @@
+// Please do not write '/' at last
+// 마지막에 '/'를 빼주세요!
+
+export const githubUrl = 'https://github.com/Averyyy';
+
+export const instagramUrl = 'https://www.instagram.com/avery_qhk';
+
+export const facebookUrl = '';
+
+export const linkedInUrl = 'https://www.linkedin.com/in/hangkai-qian-56108b1a8/';
+
+export const twitterUrl = '';
